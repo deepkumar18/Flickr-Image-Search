0b2e8397eee7855b617ec803070c712a3a43dc72
Deep <ios.deepkumar@gmail.com>
2024-09-27 08:38:47 +0530

    Initial Commit

---
+++ FlickrImageSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlickrImageSearch
+//
+//  Created by Deep kumar  on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchApp.swift
+//  FlickrImageSearch
+//
+//  Created by Deep kumar  on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
